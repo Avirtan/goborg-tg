86708c50dae58b370ce8e20939bc7ca4cfabebb3
avirtan <svinin99@mail.ru>
2023-09-25 20:58:17 +0300

    добавлено Voice

---
+++ dto/voice.go
@@ -0,0 +1,10 @@
+package dto
+
+// https://core.telegram.org/bots/api#voice
+type Voice struct {
+	FileId       string `json:"file_id"`
+	FileUniqueId string `json:"file_unique_id"`
+	Duration     int    `json:"duration"`
+	MimeType     string `json:"mime_type,omitempty"`
+	FileSize     string `json:"file_size,omitempty"`
+}
