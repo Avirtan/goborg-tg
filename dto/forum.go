99b8c8ff2e98f9a3e5763ca2da0fc68b413b3584
avirtan <svinin99@mail.ru>
2023-09-25 22:51:38 +0300

    добавлено ForumTopic

--- dto/forum.go
+++ dto/forum.go
@@ -12,3 +12,11 @@ type ForumTopicEdited struct {
 	Name              string `json:"name,omitempty"`
 	IconCustomEmojiId string `json:"icon_custom_emoji_id,omitempty"`
 }
+
+// https://core.telegram.org/bots/api#forumtopic
+type ForumTopic struct {
+	MessageThreadId   int64  `json:"message_thread_id"`
+	Name              string `json:"name"`
+	IconColor         int64  `json:"icon_color"`
+	IconCustomEmojiId string `json:"icon_custom_emoji_id"`
+}
