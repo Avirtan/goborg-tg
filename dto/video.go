d472c96423b075e3717728e530f0fdf34df37a1c
avirtan <svinin99@mail.ru>
2023-09-25 20:56:57 +0300

    добавлено VideoNote

--- dto/video.go
+++ dto/video.go
@@ -12,3 +12,13 @@ type Video struct {
 	MimeType     string    `json:"mime_type,omitempty"`
 	FileSize     string    `json:"file_size,omitempty"`
 }
+
+// https://core.telegram.org/bots/api#videonote
+type VideoNote struct {
+	FileId       string    `json:"file_id"`
+	FileUniqueId string    `json:"file_unique_id"`
+	Length       int       `json:"length"`
+	Duration     int       `json:"duration"`
+	Thumbnail    PhotoSize `json:"thumbnail,omitempty"`
+	FileSize     string    `json:"file_size,omitempty"`
+}
