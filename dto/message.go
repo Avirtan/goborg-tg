b1b0824576e1f22326462e6845e8e5ba73d3d63b
avirtan <svinin99@mail.ru>
2023-09-25 20:41:06 +0300

    добавлено MessageEntity

--- dto/message.go
+++ dto/message.go
@@ -8,6 +8,17 @@ type Message struct {
 	SenderChat      Chat   `json:"sender_chat"`
 }
 
+// https://core.telegram.org/bots/api#messageentity
+type MessageEntity struct {
+	Type          string `json:"type"`
+	Offset        int    `json:"offset"`
+	Length        int    `json:"length"`
+	Url           string `json:"url,omitempty"`
+	Language      string `json:"language,omitempty"`
+	CustomEmojiId string `json:"custom_emoji_id,omitempty"`
+	User          User   `json:"user,omitempty"`
+}
+
 type SendMessage struct {
 	ChatID      int64    `json:"chat_id"`
 	Text        string   `json:"text"`
