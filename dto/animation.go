17fc97293c97c36b7aaa180f63bb8193721ae4ef
avirtan <svinin99@mail.ru>
2023-09-25 20:48:19 +0300

    добавлено Animation

---
+++ dto/animation.go
@@ -0,0 +1,14 @@
+package dto
+
+// https://core.telegram.org/bots/api#animation
+type Animation struct {
+	FileId       string    `json:"file_id"`
+	FileUniqueId string    `json:"file_unique_id"`
+	Width        int       `json:"width"`
+	Height       int       `json:"height"`
+	Duration     int       `json:"duration"`
+	Thumbnail    PhotoSize `json:"thumbnail,omitempty"`
+	FileName     string    `json:"file_name,omitempty"`
+	MimeType     string    `json:"mime_type,omitempty"`
+	FileSize     string    `json:"file_size,omitempty"`
+}
