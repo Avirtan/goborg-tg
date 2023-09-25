da4c2e0c9b125210e7cab2a1357384588c2ada32
avirtan <svinin99@mail.ru>
2023-09-25 22:07:55 +0300

    добавлено SwitchInlineQueryChosenChat

--- dto/file.go
+++ dto/file.go
@@ -7,3 +7,12 @@ type File struct {
 	FileSize     int64  `json:"file_size,omitempty"`
 	FilePath     string `json:"file_path,omitempty"`
 }
+
+// https://core.telegram.org/bots/api#photosize
+type PhotoSize struct {
+	FileId       string `json:"file_id"`
+	FileUniqueId string `json:"file_unique_id"`
+	Width        int    `json:"width"`
+	Height       int    `json:"height"`
+	FileSize     int64  `json:"file_size"`
+}
--- dto/keyboard.go
+++ dto/keyboard.go
@@ -12,16 +12,16 @@ type InlineKeyboardMarkup struct {
 
 // https://core.telegram.org/bots/api#inlinekeyboardbutton
 type InlineKeyboardButton struct {
-	Text                         string     `json:"text"`
-	Url                          string     `json:"url,omitempty"`
-	CallbackData                 string     `json:"callback_data,omitempty"`
-	WebApp                       WebAppInfo `json:"web_app,omitempty"`
-	LoginUrl                     LoginUrl   `json:"login_url,omitempty"`
-	SwitchInlineQuery            string     `json:"switch_inline_query,omitempty"`
-	SwitchInlineQueryCurrentChat string     `json:"switch_inline_query_current_chat,omitempty"`
-	SwitchInlineQueryChosenChat  any        `json:"switch_inline_query_chosen_chat,omitempty"` // TODO SwitchInlineQueryChosenChat
-	CallbackGame                 any        `json:"callback_game,omitempty"`                   // TODO 	CallbackGame
-	Pay                          bool       `json:"pay,omitempty"`
+	Text                         string                      `json:"text"`
+	Url                          string                      `json:"url,omitempty"`
+	CallbackData                 string                      `json:"callback_data,omitempty"`
+	WebApp                       WebAppInfo                  `json:"web_app,omitempty"`
+	LoginUrl                     LoginUrl                    `json:"login_url,omitempty"`
+	SwitchInlineQuery            string                      `json:"switch_inline_query,omitempty"`
+	SwitchInlineQueryCurrentChat string                      `json:"switch_inline_query_current_chat,omitempty"`
+	SwitchInlineQueryChosenChat  SwitchInlineQueryChosenChat `json:"switch_inline_query_chosen_chat,omitempty"`
+	CallbackGame                 any                         `json:"callback_game,omitempty"` // TODO 	CallbackGame
+	Pay                          bool                        `json:"pay,omitempty"`
 }
 
 type ForceReply struct {
@@ -68,6 +68,15 @@ type KeyboardButtonRequestChat struct {
 	BotIsMember             bool  `json:"bot_is_member,omitempty"`
 	UserAdministratorRights any   `json:"user_administrator_rights,omitempty"` // TODO ChatAdministratorRights
 	BotAdministratorRights  any   `json:"bot_administrator_rights,omitempty"`  // TODO ChatAdministratorRights
+}
+
+// https://core.telegram.org/bots/api#switchinlinequerychosenchat
+type SwitchInlineQueryChosenChat struct {
+	Query             string `json:"query,omitempty"`
+	AllowUserChats    bool   `json:"allow_user_chats,omitempty"`
+	AllowBotChats     bool   `json:"allow_bot_chats,omitempty"`
+	AllowGroupChats   bool   `json:"allow_group_chats,omitempty"`
+	AllowChannelChats bool   `json:"allow_channel_chats,omitempty"`
 }
 
 // https://core.telegram.org/bots/api#keyboardbuttonpolltype
