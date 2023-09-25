cfe8d04bad60f190d1652d543441e67b88e522c7
avirtan <svinin99@mail.ru>
2023-09-25 21:41:33 +0300

    добавлено WebAppInfo

--- dto/webAppData.go
+++ dto/webAppData.go
@@ -5,3 +5,8 @@ type WebAppData struct {
 	Data       string `json:"data"`
 	ButtonText string `json:"button_text"`
 }
+
+//https://core.telegram.org/bots/api#webappinfo
+type WebAppInfo struct {
+	Url string `json:"url"`
+}
