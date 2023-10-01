958b2822b4a3c19634f3635d7d04b54bddfa34b4
avirtan <svinin99@mail.ru>
2023-10-01 19:52:27 +0300

    добавление методов

--- dto/method/message.go
+++ dto/method/message.go
@@ -2,6 +2,7 @@ package method_dto
 
 import message_dto "TGoBot/dto/message"
 
+// https://core.telegram.org/bots/api#sendmessage
 type SendMessage struct {
 	ChatID                   int64                       `json:"chat_id"`
 	Text                     string                      `json:"text"`
@@ -11,7 +12,33 @@ type SendMessage struct {
 	DisableWebPagePreview    bool                        `json:"disable_web_page_preview,omitempty"`
 	DisableNotification      bool                        `json:"disable_notification,omitempty"`
 	ProtectContent           bool                        `json:"protect_content,omitempty"`
-	ReplyToMe1ssageId        uint64                      `json:"reply_to_message_id,omitempty"`
+	ReplyToMessageId         uint64                      `json:"reply_to_message_id,omitempty"`
+	AllowSendingWithoutReply bool                        `json:"allow_sending_without_reply,omitempty"`
+	ReplyMarkup              message_dto.IKeyboard       `json:"reply_markup,omitempty"`
+}
+
+// https://core.telegram.org/bots/api#forwardmessage
+type ForwardMessage struct {
+	ChatID              int64 `json:"chat_id"`
+	FromChatId          int64 `json:"from_chat_id"`
+	MessageId           int64 `json:"message_id"`
+	MessageThreadId     int   `json:"message_thread_id,omitempty"`
+	ProtectContent      bool  `json:"protect_content,omitempty"`
+	DisableNotification bool  `json:"disable_notification,omitempty"`
+}
+
+// https://core.telegram.org/bots/api#copymessage
+type CopyMessage struct {
+	ChatID                   int64                       `json:"chat_id"`
+	FromChatId               int64                       `json:"from_chat_id"`
+	MessageId                int64                       `json:"message_id"`
+	MessageThreadId          int                         `json:"message_thread_id,omitempty"`
+	Caption                  int64                       `json:"caption,omitempty"`
+	ParseMode                string                      `json:"parse_mode,omitempty"`
+	Entities                 []message_dto.MessageEntity `json:"entities,omitempty"`
+	DisableNotification      bool                        `json:"disable_notification,omitempty"`
+	ProtectContent           bool                        `json:"protect_content,omitempty"`
+	ReplyToMessageId         uint64                      `json:"reply_to_message_id,omitempty"`
 	AllowSendingWithoutReply bool                        `json:"allow_sending_without_reply,omitempty"`
 	ReplyMarkup              message_dto.IKeyboard       `json:"reply_markup,omitempty"`
 }
--- method/message.go
+++ method/message.go
@@ -5,17 +5,52 @@ import (
 	"TGoBot/request"
 	"context"
 	"encoding/json"
-	"fmt"
 	"log/slog"
 )
 
+// https://core.telegram.org/bots/api#sendmessage
 func SendMessage(ctx context.Context, msg method_dto.SendMessage) error {
 	data, err := json.Marshal(msg)
 	if err != nil {
 		return err
 	}
-	fmt.Println(string(data))
 	response, err := request.RequestWithContextAndData(ctx, request.Get, GetUrl()+"/sendMessage", data)
+	if err != nil {
+		return err
+	}
+	responseJson, err := request.ResponseHandler(response)
+	if err != nil {
+		return err
+	}
+	slog.Debug("info", "response", responseJson)
+	return nil
+}
+
+// https://core.telegram.org/bots/api#forwardmessage
+func ForwardMessage(ctx context.Context, msg method_dto.ForwardMessage) error {
+	data, err := json.Marshal(msg)
+	if err != nil {
+		return err
+	}
+	response, err := request.RequestWithContextAndData(ctx, request.Get, GetUrl()+"/forwardMessage", data)
+	if err != nil {
+		return err
+	}
+	responseJson, err := request.ResponseHandler(response)
+	if err != nil {
+		return err
+	}
+	slog.Debug("info", "response", responseJson)
+	return nil
+}
+
+// https://core.telegram.org/bots/api#copymessage
+func CopyMessage(ctx context.Context, msg method_dto.CopyMessage) error {
+	data, err := json.Marshal(msg)
+	if err != nil {
+		return err
+	}
+	response, err := request.RequestWithContextAndData(ctx, request.Get, GetUrl()+"/copyMessage", data)
 	if err != nil {
 		return err
 	}
