7e2392fa1cb8e9c5b6085a65c38814f1a25ff2f0
avirtan <svinin99@mail.ru>
2023-10-03 11:06:38 +0300

    фикс конфликта

--- dto/method/message.go
+++ dto/method/message.go
@@ -43,6 +43,49 @@ type CopyMessage struct {
 	ReplyMarkup              message_dto.IKeyboard       `json:"reply_markup,omitempty"`
 }
 
+// https://core.telegram.org/bots/api#sendvideonote
+type SendVideoNote[ID string | int64] struct {
+	ChatID                   ID                    `json:"chat_id"`
+	VideoNote                string                `json:"video_note"`
+	MessageThreadId          int                   `json:"message_thread_id,omitempty"`
+	Duration                 int                   `json:"duration,omitempty"`
+	Length                   int                   `json:"length,omitempty"`
+	Thumbnail                string                `json:"thumbnail,omitempty"`
+	DisableNotification      bool                  `json:"disable_notification,omitempty"`
+	ProtectContent           bool                  `json:"protect_content,omitempty"`
+	ReplyToMessageId         uint64                `json:"reply_to_message_id,omitempty"`
+	AllowSendingWithoutReply bool                  `json:"allow_sending_without_reply,omitempty"`
+	ReplyMarkup              message_dto.IKeyboard `json:"reply_markup,omitempty"`
+}
+
+// https://core.telegram.org/bots/api#sendmediagroup
+type SendMediaGroup[ID string | int64] struct {
+	ChatID                   ID                       `json:"chat_id"`
+	Media                    []message_dto.InputMedia `json:"media"`
+	MessageThreadId          int                      `json:"message_thread_id,omitempty"`
+	DisableNotification      bool                     `json:"disable_notification,omitempty"`
+	ProtectContent           bool                     `json:"protect_content,omitempty"`
+	ReplyToMessageId         uint64                   `json:"reply_to_message_id,omitempty"`
+	AllowSendingWithoutReply bool                     `json:"allow_sending_without_reply,omitempty"`
+}
+
+// https://core.telegram.org/bots/api#sendlocation
+type SendLocation[ID string | int64] struct {
+	ChatID                   ID                    `json:"chat_id"`
+	Latitude                 float64               `json:"latitude"`
+	Longitude                float64               `json:"longitude"`
+	MessageThreadId          int                   `json:"message_thread_id,omitempty"`
+	HorizontalAccuracy       float32               `json:"horizontal_accuracy,omitempty"`
+	LivePeriod               int                   `json:"live_period,omitempty"`
+	Heading                  uint16                `json:"heading,omitempty"`
+	ProximityAlertRadius     uint                  `json:"proximity_alert_radius,omitempty"`
+	DisableNotification      bool                  `json:"disable_notification,omitempty"`
+	ProtectContent           bool                  `json:"protect_content,omitempty"`
+	ReplyToMessageId         uint64                `json:"reply_to_message_id,omitempty"`
+	AllowSendingWithoutReply bool                  `json:"allow_sending_without_reply,omitempty"`
+	ReplyMarkup              message_dto.IKeyboard `json:"reply_markup,omitempty"`
+}
+
 // конструктор для SendMessage
 func NewSendMessage[ID int64 | string](chatID ID, text string) *SendMessage[ID] {
 	return &SendMessage[ID]{
