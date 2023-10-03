f55ff94e805ae449a2bc2a521c5625377c224757
Yann Murr <MOXHATKA@protonmail.com>
2023-10-03 18:52:36 +0300

    поправил метод passport

--- dto/method/passport.go
+++ dto/method/passport.go
@@ -1,41 +1,16 @@
 package method_dto
 
+import passport_dto "TGoBot/dto/passport"
+
 // https://core.telegram.org/bots/api#setpassportdataerrors
-type PassportElementError struct {
-	Source      string   `json:"source"`
-	Type        string   `json:"type"`
-	FieldName   string   `json:"field_name,omitempty"`
-	DataHash    string   `json:"data_hash,omitempty"`
-	Message     string   `json:"message"`
-	FileHash    string   `json:"file_hash,omitempty"`
-	FileHashes  []string `json:"file_hashes,omitempty"`
-	ElementHash string   `json:"element_hash,omitempty"`
+type SetPassportDataErrors struct {
+	UserID int                                 `json:"user_id"`
+	Errors []passport_dto.PassportElementError `json:"errors"`
 }
 
-func NewPassportElementError(source, typeStr, message string) PassportElementError {
-	return PassportElementError{
-		Source:  source,
-		Type:    typeStr,
-		Message: message,
+func NewSetPassportDataErrors(userID int, errors []passport_dto.PassportElementError) *SetPassportDataErrors {
+	return &SetPassportDataErrors{
+		UserID: userID,
+		Errors: errors,
 	}
-}
-
-func (p *PassportElementError) SetFieldName(fieldName string) {
-	p.FieldName = fieldName
-}
-
-func (p *PassportElementError) SetDataHash(dataHash string) {
-	p.DataHash = dataHash
-}
-
-func (p *PassportElementError) SetFileHash(fileHash string) {
-	p.FileHash = fileHash
-}
-
-func (p *PassportElementError) SetFileHashes(fileHashes []string) {
-	p.FileHashes = fileHashes
-}
-
-func (p *PassportElementError) SetElementHash(elementHash string) {
-	p.ElementHash = elementHash
 }
