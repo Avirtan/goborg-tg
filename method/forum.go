1f87e0955271e4d9a3a65aadbe34f37283ab8819
gggallahad <gggallahad@gmail.com>
2023-10-05 14:09:13 +0300

    добавил метод ReopenGeneralForumTopic

--- method/forum.go
+++ method/forum.go
@@ -125,3 +125,16 @@ func CloseGeneralForumTopic[ID int64 | string](ctx context.Context) (*bool, erro
 	slog.Debug("info", "response", responseJson)
 	return responseJson, nil
 }
+
+func ReopenGeneralForumTopic[ID int64 | string](ctx context.Context) (*bool, error) {
+	response, err := request.RequestWithContext(ctx, request.Get, GetUrl()+"/reopenGeneralForumTopic")
+	if err != nil {
+		return nil, err
+	}
+	responseJson, err := request.ResponseHandlerToType[bool](response)
+	if err != nil {
+		return nil, err
+	}
+	slog.Debug("info", "response", responseJson)
+	return responseJson, nil
+}
