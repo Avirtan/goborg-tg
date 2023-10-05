5ac91f94c587f9c7bccce6c50e2ad082f9fc7952
gggallahad <gggallahad@gmail.com>
2023-10-05 11:21:11 +0300

    добавил метод GetChatAdministrators

--- method/chat.go
+++ method/chat.go
@@ -21,4 +21,15 @@ func GetChat[ID int64 | string](ctx context.Context) (*message_dto.Chat, error) 
 	return responseJson, nil
 }
 
-func 
+func GetChatAdministrators[ID int64 | string](ctx context.Context) (*message_dto.ChatMember, error) {
+	response, err := request.RequestWithContext(ctx, request.Get, GetUrl()+"/getChatAdministrators")
+	if err != nil {
+		return nil, err
+	}
+	responseJson, err := request.ResponseHandlerToType[message_dto.ChatMember](response)
+	if err != nil {
+		return nil, err
+	}
+	slog.Debug("info", "response", responseJson)
+	return responseJson, nil
+}
