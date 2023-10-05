fa42c8cb6d5ccfef39d1a3522c27925175f1dd16
gggallahad <gggallahad@gmail.com>
2023-10-05 14:07:09 +0300

    добавил метод CloseGeneralForumTopic

--- method/forum.go
+++ method/forum.go
@@ -112,3 +112,16 @@ func EditGeneralForumTopic[ID int64 | string](ctx context.Context, name string) 
 	slog.Debug("info", "response", responseJson)
 	return responseJson, nil
 }
+
+func CloseGeneralForumTopic[ID int64 | string](ctx context.Context) (*bool, error) {
+	response, err := request.RequestWithContext(ctx, request.Get, GetUrl()+"/closeGeneralForumTopic")
+	if err != nil {
+		return nil, err
+	}
+	responseJson, err := request.ResponseHandlerToType[bool](response)
+	if err != nil {
+		return nil, err
+	}
+	slog.Debug("info", "response", responseJson)
+	return responseJson, nil
+}
