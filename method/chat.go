1836837b7932f0fac12fa7d501e24033e18cba9a
gggallahad <gggallahad@gmail.com>
2023-10-05 11:55:29 +0300

    добавил метод GetChatMember

--- method/chat.go
+++ method/chat.go
@@ -46,3 +46,16 @@ func GetChatMemberCount[ID int64 | string](ctx context.Context) (*int64, error) 
 	slog.Debug("info", "response", responseJson)
 	return responseJson, nil
 }
+
+func GetChatMember[ID int64 | string](ctx context.Context, userId int64) (*message_dto.ChatMember, error) {
+	response, err := request.RequestWithContext(ctx, request.Get, GetUrl()+"/getChatMember")
+	if err != nil {
+		return nil, err
+	}
+	responseJson, err := request.ResponseHandlerToType[message_dto.ChatMember](response)
+	if err != nil {
+		return nil, err
+	}
+	slog.Debug("info", "response", responseJson)
+	return responseJson, nil
+}
