2a80643c34781bdd4a00e60eaa1f9d9df0ddc817
gggallahad <gggallahad@gmail.com>
2023-10-05 11:13:32 +0300

    добавил метод GetChat

---
+++ method/chat.go
@@ -0,0 +1,24 @@
+package method
+
+import (
+	"context"
+	"log/slog"
+
+	message_dto "github.com/Avirtan/TGoBot/dto/message"
+	"github.com/Avirtan/TGoBot/request"
+)
+
+func GetChat[ID int64 | string](ctx context.Context) (*message_dto.Chat, error) {
+	response, err := request.RequestWithContext(ctx, request.Get, GetUrl()+"/getChat")
+	if err != nil {
+		return nil, err
+	}
+	responseJson, err := request.ResponseHandlerToType[message_dto.Chat](response)
+	if err != nil {
+		return nil, err
+	}
+	slog.Debug("info", "response", responseJson)
+	return responseJson, nil
+}
+
+func 
