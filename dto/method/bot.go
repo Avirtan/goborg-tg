16b0d2bccff84a4a7dbacaa9e49d6c44bf3ac6c0
Yann Murr <MOXHATKA@protonmail.com>
2023-10-07 17:41:18 +0300

    билдеры для bot

--- dto/method/bot.go
+++ dto/method/bot.go
@@ -7,15 +7,47 @@ type GetMyDefaultAdministratorRights struct {
 	ForChannels bool `json:"for_channels,omitempty"`
 }
 
+func NewGetMyDefaultAdministratorRights() *GetMyDefaultAdministratorRights {
+	return &GetMyDefaultAdministratorRights{}
+}
+
+func (g *GetMyDefaultAdministratorRights) SetForChannels(forChannels bool) *GetMyDefaultAdministratorRights {
+	g.ForChannels = forChannels
+	return g
+}
+
 // https://core.telegram.org/bots/api#setmydefaultadministratorrights
 type SetMyDefaultAdministratorRights struct {
 	Rights      message_dto.ChatAdministratorRights `json:"rights,omitempty"`
 	ForChannels bool                                `json:"for_channels,omitempty"`
 }
 
+func NewSetMyDefaultAdministratorRights() *SetMyDefaultAdministratorRights {
+	return &SetMyDefaultAdministratorRights{}
+}
+
+func (s *SetMyDefaultAdministratorRights) SetRights(rights message_dto.ChatAdministratorRights) *SetMyDefaultAdministratorRights {
+	s.Rights = rights
+	return s
+}
+
+func (s *SetMyDefaultAdministratorRights) SetForChannels(forChannels bool) *SetMyDefaultAdministratorRights {
+	s.ForChannels = forChannels
+	return s
+}
+
 // https://core.telegram.org/bots/api#getchatmenubutton
 type GetChatMenuButton struct {
 	ChatId uint64 `json:"chat_id,omitempty"`
+}
+
+func NewGetChatMenuButton() *GetChatMenuButton {
+	return &GetChatMenuButton{}
+}
+
+func (g *GetChatMenuButton) SetChatId(chatId uint64) *GetChatMenuButton {
+	g.ChatId = chatId
+	return g
 }
 
 // https://core.telegram.org/bots/api#setchatmenubutton
@@ -24,9 +56,32 @@ type SetChatMenuButton struct {
 	MenuButton message_dto.MenuButton `json:"menu_button,omitempty"`
 }
 
+func NewSetChatMenuButton() *SetChatMenuButton {
+	return &SetChatMenuButton{}
+}
+
+func (s *SetChatMenuButton) SetChatId(chatId uint64) *SetChatMenuButton {
+	s.ChatId = chatId
+	return s
+}
+
+func (s *SetChatMenuButton) SetMenuButton(menuButton message_dto.MenuButton) *SetChatMenuButton {
+	s.MenuButton = menuButton
+	return s
+}
+
 // https://core.telegram.org/bots/api#getmyshortdescription
 type GetMyShortDescription struct {
 	LanguageCode string `json:"language_code,omitempty"`
+}
+
+func NewGetMyShortDescription() *GetMyShortDescription {
+	return &GetMyShortDescription{}
+}
+
+func (g *GetMyShortDescription) SetLanguageCode(languageCode string) *GetMyShortDescription {
+	g.LanguageCode = languageCode
+	return g
 }
 
 // https://core.telegram.org/bots/api#setmyshortdescription
@@ -35,9 +90,32 @@ type SetMyShortDescription struct {
 	LanguageCode     string `json:"language_code,omitempty"`
 }
 
+func NewSetMyShortDescription() *SetMyShortDescription {
+	return &SetMyShortDescription{}
+}
+
+func (s *SetMyShortDescription) SetShortDescription(shortDescription string) *SetMyShortDescription {
+	s.ShortDescription = shortDescription
+	return s
+}
+
+func (s *SetMyShortDescription) SetLanguageCode(languageCode string) *SetMyShortDescription {
+	s.LanguageCode = languageCode
+	return s
+}
+
 // https://core.telegram.org/bots/api#getmydescription
 type GetMyDescription struct {
 	LanguageCode string `json:"language_code,omitempty"`
+}
+
+func NewGetMyDescription() *GetMyDescription {
+	return &GetMyDescription{}
+}
+
+func (g *GetMyDescription) SetLanguageCode(languageCode string) *GetMyDescription {
+	g.LanguageCode = languageCode
+	return g
 }
 
 // https://core.telegram.org/bots/api#setmydescription
@@ -46,9 +124,32 @@ type SetMyDescription struct {
 	LanguageCode string `json:"language_code,omitempty"`
 }
 
+func NewSetMyDescription() *SetMyDescription {
+	return &SetMyDescription{}
+}
+
+func (s *SetMyDescription) SetDescription(description string) *SetMyDescription {
+	s.Description = description
+	return s
+}
+
+func (s *SetMyDescription) SetLanguageCode(languageCode string) *SetMyDescription {
+	s.LanguageCode = languageCode
+	return s
+}
+
 // https://core.telegram.org/bots/api#getmyname
 type GetMyName struct {
 	LanguageCode string `json:"language_code,omitempty"`
+}
+
+func NewGetMyName() *GetMyName {
+	return &GetMyName{}
+}
+
+func (g *GetMyName) SetLanguageCode(languageCode string) *GetMyName {
+	g.LanguageCode = languageCode
+	return g
 }
 
 // https://core.telegram.org/bots/api#setmyname
@@ -57,7 +158,30 @@ type SetMyName struct {
 	LanguageCode string `json:"language_code,omitempty"`
 }
 
+func NewSetMyName() *SetMyName {
+	return &SetMyName{}
+}
+
+func (s *SetMyName) SetName(name string) *SetMyName {
+	s.Name = name
+	return s
+}
+
+func (s *SetMyName) SetLanguageCode(languageCode string) *SetMyName {
+	s.LanguageCode = languageCode
+	return s
+}
+
 // https://core.telegram.org/bots/api#getfile
 type GetFile struct {
 	FileId string `json:"file_id,omitempty"`
+}
+
+func NewGetFile() *GetFile {
+	return &GetFile{}
+}
+
+func (g *GetFile) SetFileId(fileId string) *GetFile {
+	g.FileId = fileId
+	return g
 }
