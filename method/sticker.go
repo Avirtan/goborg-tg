49cc8d9955ab019887cb7bcdafaf6d89c02366b4
Yann Murr <MOXHATKA@protonmail.com>
2023-10-07 19:43:10 +0300

    рефакторинг sticker

--- method/sticker.go
+++ method/sticker.go
@@ -13,331 +13,331 @@ import (
 )
 
 // https://core.telegram.org/bots/api#sendsticker
-func SendSticker[ID int64 | string](ctx context.Context, ss method_dto.SendSticker[ID]) (*message_dto.Message, error) {
-	data, err := json.Marshal(ss)
+func SendSticker[ID int64 | string](ctx context.Context, data method_dto.SendSticker[ID]) (*message_dto.Message, error) {
+	marshalBytes, err := json.Marshal(data)
 	if err != nil {
 		return nil, err
 	}
 
-	response, err := request.RequestWithContextAndData(ctx, request.Get, GetUrl()+"/sendSticker", data)
+	response, err := request.RequestWithContextAndData(ctx, request.Get, GetUrl()+"/sendSticker", marshalBytes)
 	if err != nil {
 		return nil, err
 	}
 
-	responseJson, err := request.ResponseHandlerToType[message_dto.Message](response)
+	responseData, err := request.ResponseHandlerToType[message_dto.Message](response)
 	if err != nil {
 		return nil, err
 	}
 
-	slog.Debug("info", "response", responseJson)
+	slog.Debug("info", "response", responseData)
 
-	return responseJson, nil
+	return responseData, nil
 }
 
 // https://core.telegram.org/bots/api#getstickerset
-func GetStickerSet(ctx context.Context, gss method_dto.GetStickerSet) (*stickers_dto.StickerSet, error) {
-	data, err := json.Marshal(gss)
+func GetStickerSet(ctx context.Context, data method_dto.GetStickerSet) (*stickers_dto.StickerSet, error) {
+	marshalBytes, err := json.Marshal(data)
 	if err != nil {
 		return nil, err
 	}
 
-	response, err := request.RequestWithContextAndData(ctx, request.Get, GetUrl()+"/getStickerSet", data)
+	response, err := request.RequestWithContextAndData(ctx, request.Get, GetUrl()+"/getStickerSet", marshalBytes)
 	if err != nil {
 		return nil, err
 	}
 
-	responseJson, err := request.ResponseHandlerToType[stickers_dto.StickerSet](response)
+	responseData, err := request.ResponseHandlerToType[stickers_dto.StickerSet](response)
 	if err != nil {
 		return nil, err
 	}
 
-	slog.Debug("info", "response", responseJson)
+	slog.Debug("info", "response", responseData)
 
-	return responseJson, nil
+	return responseData, nil
 }
 
 // https://core.telegram.org/bots/api#getcustomemojistickers
-func GetCustomEmojiStickers(ctx context.Context, gces method_dto.GetCustomEmojiStickers) (*[]stickers_dto.Sticker, error) {
-	data, err := json.Marshal(gces)
+func GetCustomEmojiStickers(ctx context.Context, data method_dto.GetCustomEmojiStickers) (*[]stickers_dto.Sticker, error) {
+	marshalBytes, err := json.Marshal(data)
 	if err != nil {
 		return nil, err
 	}
 
-	response, err := request.RequestWithContextAndData(ctx, request.Get, GetUrl()+"/getCustomEmojiStickers", data)
+	response, err := request.RequestWithContextAndData(ctx, request.Get, GetUrl()+"/getCustomEmojiStickers", marshalBytes)
 	if err != nil {
 		return nil, err
 	}
 
-	responseJson, err := request.ResponseHandlerToType[[]stickers_dto.Sticker](response)
+	responseData, err := request.ResponseHandlerToType[[]stickers_dto.Sticker](response)
 	if err != nil {
 		return nil, err
 	}
 
-	slog.Debug("info", "response", responseJson)
+	slog.Debug("info", "response", responseData)
 
-	return responseJson, nil
+	return responseData, nil
 }
 
 // https://core.telegram.org/bots/api#uploadstickerfile
-func UploadStickerFile(ctx context.Context, usf method_dto.UploadStickerFile) (*utils_dto.File, error) {
-	data, err := json.Marshal(usf)
+func UploadStickerFile(ctx context.Context, data method_dto.UploadStickerFile) (*utils_dto.File, error) {
+	marshalBytes, err := json.Marshal(data)
 	if err != nil {
 		return nil, err
 	}
 
-	response, err := request.RequestWithContextAndData(ctx, request.Get, GetUrl()+"/uploadStickerFile", data)
+	response, err := request.RequestWithContextAndData(ctx, request.Get, GetUrl()+"/uploadStickerFile", marshalBytes)
 	if err != nil {
 		return nil, err
 	}
 
-	responseJson, err := request.ResponseHandlerToType[utils_dto.File](response)
+	responseData, err := request.ResponseHandlerToType[utils_dto.File](response)
 	if err != nil {
 		return nil, err
 	}
 
-	slog.Debug("info", "response", responseJson)
+	slog.Debug("info", "response", responseData)
 
-	return responseJson, nil
+	return responseData, nil
 }
 
 // https://core.telegram.org/bots/api#createnewstickerset
-func CreateNewStickerSet(ctx context.Context, usf method_dto.CreateNewStickerSet) (*bool, error) {
-	data, err := json.Marshal(usf)
+func CreateNewStickerSet(ctx context.Context, data method_dto.CreateNewStickerSet) (*bool, error) {
+	marshalBytes, err := json.Marshal(data)
 	if err != nil {
 		return nil, err
 	}
 
-	response, err := request.RequestWithContextAndData(ctx, request.Get, GetUrl()+"/createNewStickerSet", data)
+	response, err := request.RequestWithContextAndData(ctx, request.Get, GetUrl()+"/createNewStickerSet", marshalBytes)
 	if err != nil {
 		return nil, err
 	}
 
-	responseJson, err := request.ResponseHandlerToType[bool](response)
+	responseData, err := request.ResponseHandlerToType[bool](response)
 	if err != nil {
 		return nil, err
 	}
 
-	slog.Debug("info", "response", responseJson)
+	slog.Debug("info", "response", responseData)
 
-	return responseJson, nil
+	return responseData, nil
 }
 
 // https://core.telegram.org/bots/api#addstickertoset
-func AddStickerToSet(ctx context.Context, asts method_dto.AddStickerToSet) (*bool, error) {
-	data, err := json.Marshal(asts)
+func AddStickerToSet(ctx context.Context, data method_dto.AddStickerToSet) (*bool, error) {
+	marshalBytes, err := json.Marshal(data)
 	if err != nil {
 		return nil, err
 	}
 
-	response, err := request.RequestWithContextAndData(ctx, request.Get, GetUrl()+"/addStickerToSet", data)
+	response, err := request.RequestWithContextAndData(ctx, request.Get, GetUrl()+"/addStickerToSet", marshalBytes)
 	if err != nil {
 		return nil, err
 	}
 
-	responseJson, err := request.ResponseHandlerToType[bool](response)
+	responseData, err := request.ResponseHandlerToType[bool](response)
 	if err != nil {
 		return nil, err
 	}
 
-	slog.Debug("info", "response", responseJson)
+	slog.Debug("info", "response", responseData)
 
-	return responseJson, nil
+	return responseData, nil
 }
 
 // https://core.telegram.org/bots/api#setstickerpositioninset
-func SetStickerPositionInSet(ctx context.Context, sspis method_dto.SetStickerPositionInSet) (*bool, error) {
-	data, err := json.Marshal(sspis)
+func SetStickerPositionInSet(ctx context.Context, data method_dto.SetStickerPositionInSet) (*bool, error) {
+	marshalBytes, err := json.Marshal(data)
 	if err != nil {
 		return nil, err
 	}
 
-	response, err := request.RequestWithContextAndData(ctx, request.Get, GetUrl()+"/setStickerPositionInSet", data)
+	response, err := request.RequestWithContextAndData(ctx, request.Get, GetUrl()+"/setStickerPositionInSet", marshalBytes)
 	if err != nil {
 		return nil, err
 	}
 
-	responseJson, err := request.ResponseHandlerToType[bool](response)
+	responseData, err := request.ResponseHandlerToType[bool](response)
 	if err != nil {
 		return nil, err
 	}
 
-	slog.Debug("info", "response", responseJson)
+	slog.Debug("info", "response", responseData)
 
-	return responseJson, nil
+	return responseData, nil
 }
 
 // https://core.telegram.org/bots/api#deletestickerfromset
-func DeleteStickerFromSet(ctx context.Context, dsfs method_dto.DeleteStickerFromSet) (*bool, error) {
-	data, err := json.Marshal(dsfs)
+func DeleteStickerFromSet(ctx context.Context, data method_dto.DeleteStickerFromSet) (*bool, error) {
+	marshalBytes, err := json.Marshal(data)
 	if err != nil {
 		return nil, err
 	}
 
-	response, err := request.RequestWithContextAndData(ctx, request.Get, GetUrl()+"/deleteStickerFromSet", data)
+	response, err := request.RequestWithContextAndData(ctx, request.Get, GetUrl()+"/deleteStickerFromSet", marshalBytes)
 	if err != nil {
 		return nil, err
 	}
 
-	responseJson, err := request.ResponseHandlerToType[bool](response)
+	responseData, err := request.ResponseHandlerToType[bool](response)
 	if err != nil {
 		return nil, err
 	}
 
-	slog.Debug("info", "response", responseJson)
+	slog.Debug("info", "response", responseData)
 
-	return responseJson, nil
+	return responseData, nil
 }
 
 // https://core.telegram.org/bots/api#setstickeremojilist
-func SetStickerEmojiList(ctx context.Context, ssel method_dto.SetStickerEmojiList) (*bool, error) {
-	data, err := json.Marshal(ssel)
+func SetStickerEmojiList(ctx context.Context, data method_dto.SetStickerEmojiList) (*bool, error) {
+	marshalBytes, err := json.Marshal(data)
 	if err != nil {
 		return nil, err
 	}
 
-	response, err := request.RequestWithContextAndData(ctx, request.Get, GetUrl()+"/setStickerEmojiList", data)
+	response, err := request.RequestWithContextAndData(ctx, request.Get, GetUrl()+"/setStickerEmojiList", marshalBytes)
 	if err != nil {
 		return nil, err
 	}
 
-	responseJson, err := request.ResponseHandlerToType[bool](response)
+	responseData, err := request.ResponseHandlerToType[bool](response)
 	if err != nil {
 		return nil, err
 	}
 
-	slog.Debug("info", "response", responseJson)
+	slog.Debug("info", "response", responseData)
 
-	return responseJson, nil
+	return responseData, nil
 }
 
 // https://core.telegram.org/bots/api#setstickerkeywords
-func SetStickerKeywords(ctx context.Context, ssk method_dto.SetStickerKeywords) (*bool, error) {
-	data, err := json.Marshal(ssk)
+func SetStickerKeywords(ctx context.Context, data method_dto.SetStickerKeywords) (*bool, error) {
+	marshalBytes, err := json.Marshal(data)
 	if err != nil {
 		return nil, err
 	}
 
-	response, err := request.RequestWithContextAndData(ctx, request.Get, GetUrl()+"/setStickerKeywords", data)
+	response, err := request.RequestWithContextAndData(ctx, request.Get, GetUrl()+"/setStickerKeywords", marshalBytes)
 	if err != nil {
 		return nil, err
 	}
 
-	responseJson, err := request.ResponseHandlerToType[bool](response)
+	responseData, err := request.ResponseHandlerToType[bool](response)
 	if err != nil {
 		return nil, err
 	}
 
-	slog.Debug("info", "response", responseJson)
+	slog.Debug("info", "response", responseData)
 
-	return responseJson, nil
+	return responseData, nil
 }
 
 // https://core.telegram.org/bots/api#setstickermaskposition
-func SetStickerMaskPosition(ctx context.Context, ssmp method_dto.SetStickerMaskPosition) (*bool, error) {
-	data, err := json.Marshal(ssmp)
+func SetStickerMaskPosition(ctx context.Context, data method_dto.SetStickerMaskPosition) (*bool, error) {
+	marshalBytes, err := json.Marshal(data)
 	if err != nil {
 		return nil, err
 	}
 
-	response, err := request.RequestWithContextAndData(ctx, request.Get, GetUrl()+"/setStickerMaskPosition", data)
+	response, err := request.RequestWithContextAndData(ctx, request.Get, GetUrl()+"/setStickerMaskPosition", marshalBytes)
 	if err != nil {
 		return nil, err
 	}
 
-	responseJson, err := request.ResponseHandlerToType[bool](response)
+	responseData, err := request.ResponseHandlerToType[bool](response)
 	if err != nil {
 		return nil, err
 	}
 
-	slog.Debug("info", "response", responseJson)
+	slog.Debug("info", "response", responseData)
 
-	return responseJson, nil
+	return responseData, nil
 }
 
 // https://core.telegram.org/bots/api#setstickersettitle
-func SetStickerSetTitle(ctx context.Context, ssst method_dto.SetStickerSetTitle) (*bool, error) {
-	data, err := json.Marshal(ssst)
+func SetStickerSetTitle(ctx context.Context, data method_dto.SetStickerSetTitle) (*bool, error) {
+	marshalBytes, err := json.Marshal(data)
 	if err != nil {
 		return nil, err
 	}
 
-	response, err := request.RequestWithContextAndData(ctx, request.Get, GetUrl()+"/setStickerSetTitle", data)
+	response, err := request.RequestWithContextAndData(ctx, request.Get, GetUrl()+"/setStickerSetTitle", marshalBytes)
 	if err != nil {
 		return nil, err
 	}
 
-	responseJson, err := request.ResponseHandlerToType[bool](response)
+	responseData, err := request.ResponseHandlerToType[bool](response)
 	if err != nil {
 		return nil, err
 	}
 
-	slog.Debug("info", "response", responseJson)
+	slog.Debug("info", "response", responseData)
 
-	return responseJson, nil
+	return responseData, nil
 }
 
 // https://core.telegram.org/bots/api#setstickersetthumbnail
-func SetStickerSetThumbnail(ctx context.Context, ssst method_dto.SetStickerSetThumbnail) (*bool, error) {
-	data, err := json.Marshal(ssst)
+func SetStickerSetThumbnail(ctx context.Context, data method_dto.SetStickerSetThumbnail) (*bool, error) {
+	marshalBytes, err := json.Marshal(data)
 	if err != nil {
 		return nil, err
 	}
 
-	response, err := request.RequestWithContextAndData(ctx, request.Get, GetUrl()+"/setStickerSetThumbnail", data)
+	response, err := request.RequestWithContextAndData(ctx, request.Get, GetUrl()+"/setStickerSetThumbnail", marshalBytes)
 	if err != nil {
 		return nil, err
 	}
 
-	responseJson, err := request.ResponseHandlerToType[bool](response)
+	responseData, err := request.ResponseHandlerToType[bool](response)
 	if err != nil {
 		return nil, err
 	}
 
-	slog.Debug("info", "response", responseJson)
+	slog.Debug("info", "response", responseData)
 
-	return responseJson, nil
+	return responseData, nil
 }
 
 // https://core.telegram.org/bots/api#setcustomemojistickersetthumbnail
-func SetCustomEmojiStickerSetThumbnail(ctx context.Context, scesst method_dto.SetCustomEmojiStickerSetThumbnail) (*bool, error) {
-	data, err := json.Marshal(scesst)
+func SetCustomEmojiStickerSetThumbnail(ctx context.Context, data method_dto.SetCustomEmojiStickerSetThumbnail) (*bool, error) {
+	marshalBytes, err := json.Marshal(data)
 	if err != nil {
 		return nil, err
 	}
 
-	response, err := request.RequestWithContextAndData(ctx, request.Get, GetUrl()+"/setCustomEmojiStickerSetThumbnail", data)
+	response, err := request.RequestWithContextAndData(ctx, request.Get, GetUrl()+"/setCustomEmojiStickerSetThumbnail", marshalBytes)
 	if err != nil {
 		return nil, err
 	}
 
-	responseJson, err := request.ResponseHandlerToType[bool](response)
+	responseData, err := request.ResponseHandlerToType[bool](response)
 	if err != nil {
 		return nil, err
 	}
 
-	slog.Debug("info", "response", responseJson)
+	slog.Debug("info", "response", responseData)
 
-	return responseJson, nil
+	return responseData, nil
 }
 
 // https://core.telegram.org/bots/api#deletestickerset
-func DeleteStickerSet(ctx context.Context, dss method_dto.DeleteStickerSet) (*bool, error) {
-	data, err := json.Marshal(dss)
+func DeleteStickerSet(ctx context.Context, data method_dto.DeleteStickerSet) (*bool, error) {
+	marshalBytes, err := json.Marshal(data)
 	if err != nil {
 		return nil, err
 	}
 
-	response, err := request.RequestWithContextAndData(ctx, request.Get, GetUrl()+"/deleteStickerSet", data)
+	response, err := request.RequestWithContextAndData(ctx, request.Get, GetUrl()+"/deleteStickerSet", marshalBytes)
 	if err != nil {
 		return nil, err
 	}
 
-	responseJson, err := request.ResponseHandlerToType[bool](response)
+	responseData, err := request.ResponseHandlerToType[bool](response)
 	if err != nil {
 		return nil, err
 	}
 
-	slog.Debug("info", "response", responseJson)
+	slog.Debug("info", "response", responseData)
 
-	return responseJson, nil
+	return responseData, nil
 }
