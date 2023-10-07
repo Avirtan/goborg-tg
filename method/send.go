455ea5cb61e29cd61022f23d9ce9c6a16353e930
Yann Murr <MOXHATKA@protonmail.com>
2023-10-07 19:36:51 +0300

    рефакторинг send

--- method/send.go
+++ method/send.go
@@ -10,199 +10,243 @@ import (
 )
 
 // https://core.telegram.org/bots/api#sendphoto
-func SendPhoto[ID int64 | string](ctx context.Context, msg method_dto.SendPhoto[ID]) error {
-	data, err := json.Marshal(msg)
+func SendPhoto[ID int64 | string](ctx context.Context, data method_dto.SendPhoto[ID]) error {
+	marshalBytes, err := json.Marshal(data)
 	if err != nil {
 		return err
 	}
-	response, err := request.RequestWithContextAndData(ctx, request.Get, GetUrl()+"/sendPhoto", data)
+
+	response, err := request.RequestWithContextAndData(ctx, request.Get, GetUrl()+"/sendPhoto", marshalBytes)
 	if err != nil {
 		return err
 	}
-	responseJson, err := request.ResponseHandler(response)
+
+	responseData, err := request.ResponseHandler(response)
 	if err != nil {
 		return err
 	}
-	slog.Debug("info", "response", responseJson)
+
+	slog.Debug("info", "response", responseData)
+
 	return nil
 }
 
 // https://core.telegram.org/bots/api#sendaudio
-func SendAudio[ID int64 | string](ctx context.Context, msg method_dto.SendAudio[ID]) error {
-	data, err := json.Marshal(msg)
+func SendAudio[ID int64 | string](ctx context.Context, data method_dto.SendAudio[ID]) error {
+	marshalBytes, err := json.Marshal(data)
 	if err != nil {
 		return err
 	}
-	response, err := request.RequestWithContextAndData(ctx, request.Get, GetUrl()+"/sendAudio", data)
+
+	response, err := request.RequestWithContextAndData(ctx, request.Get, GetUrl()+"/sendAudio", marshalBytes)
 	if err != nil {
 		return err
 	}
-	responseJson, err := request.ResponseHandler(response)
+
+	responseData, err := request.ResponseHandler(response)
 	if err != nil {
 		return err
 	}
-	slog.Debug("info", "response", responseJson)
+
+	slog.Debug("info", "response", responseData)
+
 	return nil
 }
 
 // https://core.telegram.org/bots/api#senddocument
-func SendDocument[ID int64 | string](ctx context.Context, msg method_dto.SendDocument[ID]) error {
-	data, err := json.Marshal(msg)
+func SendDocument[ID int64 | string](ctx context.Context, data method_dto.SendDocument[ID]) error {
+	marshalBytes, err := json.Marshal(data)
 	if err != nil {
 		return err
 	}
-	response, err := request.RequestWithContextAndData(ctx, request.Get, GetUrl()+"/sendDocument", data)
+
+	response, err := request.RequestWithContextAndData(ctx, request.Get, GetUrl()+"/sendDocument", marshalBytes)
 	if err != nil {
 		return err
 	}
-	responseJson, err := request.ResponseHandler(response)
+
+	responseData, err := request.ResponseHandler(response)
 	if err != nil {
 		return err
 	}
-	slog.Debug("info", "response", responseJson)
+
+	slog.Debug("info", "response", responseData)
+
 	return nil
 }
 
 // https://core.telegram.org/bots/api#sendvideo
-func SendVideo[ID int64 | string](ctx context.Context, msg method_dto.SendVideo[ID]) error {
-	data, err := json.Marshal(msg)
+func SendVideo[ID int64 | string](ctx context.Context, data method_dto.SendVideo[ID]) error {
+	marshalBytes, err := json.Marshal(data)
 	if err != nil {
 		return err
 	}
-	response, err := request.RequestWithContextAndData(ctx, request.Get, GetUrl()+"/sendVideo", data)
+
+	response, err := request.RequestWithContextAndData(ctx, request.Get, GetUrl()+"/sendVideo", marshalBytes)
 	if err != nil {
 		return err
 	}
-	responseJson, err := request.ResponseHandler(response)
+
+	responseData, err := request.ResponseHandler(response)
 	if err != nil {
 		return err
 	}
-	slog.Debug("info", "response", responseJson)
+
+	slog.Debug("info", "response", responseData)
+
 	return nil
 }
 
 // https://core.telegram.org/bots/api#sendanimation
-func SendAnimation[ID int64 | string](ctx context.Context, msg method_dto.SendAnimation[ID]) error {
-	data, err := json.Marshal(msg)
+func SendAnimation[ID int64 | string](ctx context.Context, data method_dto.SendAnimation[ID]) error {
+	marshalBytes, err := json.Marshal(data)
 	if err != nil {
 		return err
 	}
-	response, err := request.RequestWithContextAndData(ctx, request.Get, GetUrl()+"/sendAnimation", data)
+
+	response, err := request.RequestWithContextAndData(ctx, request.Get, GetUrl()+"/sendAnimation", marshalBytes)
 	if err != nil {
 		return err
 	}
-	responseJson, err := request.ResponseHandler(response)
+
+	responseData, err := request.ResponseHandler(response)
 	if err != nil {
 		return err
 	}
-	slog.Debug("info", "response", responseJson)
+
+	slog.Debug("info", "response", responseData)
+
 	return nil
 }
 
 // https://core.telegram.org/bots/api#sendvoice
-func SendVoice[ID int64 | string](ctx context.Context, msg method_dto.SendVoice[ID]) error {
-	data, err := json.Marshal(msg)
+func SendVoice[ID int64 | string](ctx context.Context, data method_dto.SendVoice[ID]) error {
+	marshalBytes, err := json.Marshal(data)
 	if err != nil {
 		return err
 	}
-	response, err := request.RequestWithContextAndData(ctx, request.Get, GetUrl()+"/sendVoice", data)
+
+	response, err := request.RequestWithContextAndData(ctx, request.Get, GetUrl()+"/sendVoice", marshalBytes)
 	if err != nil {
 		return err
 	}
-	responseJson, err := request.ResponseHandler(response)
+
+	responseData, err := request.ResponseHandler(response)
 	if err != nil {
 		return err
 	}
-	slog.Debug("info", "response", responseJson)
+
+	slog.Debug("info", "response", responseData)
+
 	return nil
 }
 
 // https://core.telegram.org/bots/api#sendvideonote
-func SendVideoNote[ID int64 | string](ctx context.Context, msg method_dto.SendVideoNote[ID]) error {
-	data, err := json.Marshal(msg)
+func SendVideoNote[ID int64 | string](ctx context.Context, data method_dto.SendVideoNote[ID]) error {
+	marshalBytes, err := json.Marshal(data)
 	if err != nil {
 		return err
 	}
-	response, err := request.RequestWithContextAndData(ctx, request.Get, GetUrl()+"/sendVideoNote", data)
+
+	response, err := request.RequestWithContextAndData(ctx, request.Get, GetUrl()+"/sendVideoNote", marshalBytes)
 	if err != nil {
 		return err
 	}
-	responseJson, err := request.ResponseHandler(response)
+
+	responseData, err := request.ResponseHandler(response)
 	if err != nil {
 		return err
 	}
-	slog.Debug("info", "response", responseJson)
+
+	slog.Debug("info", "response", responseData)
+
 	return nil
 }
 
 // https://core.telegram.org/bots/api#sendmediagroup
-func SendMediaGroup[ID int64 | string](ctx context.Context, msg method_dto.SendMediaGroup[ID]) error {
-	data, err := json.Marshal(msg)
+func SendMediaGroup[ID int64 | string](ctx context.Context, data method_dto.SendMediaGroup[ID]) error {
+	marshalBytes, err := json.Marshal(data)
 	if err != nil {
 		return err
 	}
-	response, err := request.RequestWithContextAndData(ctx, request.Get, GetUrl()+"/sendMediaGroup", data)
+
+	response, err := request.RequestWithContextAndData(ctx, request.Get, GetUrl()+"/sendMediaGroup", marshalBytes)
 	if err != nil {
 		return err
 	}
-	responseJson, err := request.ResponseHandler(response)
+
+	responseData, err := request.ResponseHandler(response)
 	if err != nil {
 		return err
 	}
-	slog.Debug("info", "response", responseJson)
+
+	slog.Debug("info", "response", responseData)
+
 	return nil
 }
 
 // https://core.telegram.org/bots/api#sendlocation
-func SendLocation[ID int64 | string](ctx context.Context, msg method_dto.SendLocation[ID]) error {
-	data, err := json.Marshal(msg)
+func SendLocation[ID int64 | string](ctx context.Context, data method_dto.SendLocation[ID]) error {
+	marshalBytes, err := json.Marshal(data)
 	if err != nil {
 		return err
 	}
-	response, err := request.RequestWithContextAndData(ctx, request.Get, GetUrl()+"/sendLocation", data)
+
+	response, err := request.RequestWithContextAndData(ctx, request.Get, GetUrl()+"/sendLocation", marshalBytes)
 	if err != nil {
 		return err
 	}
-	responseJson, err := request.ResponseHandler(response)
+
+	responseData, err := request.ResponseHandler(response)
 	if err != nil {
 		return err
 	}
-	slog.Debug("info", "response", responseJson)
+
+	slog.Debug("info", "response", responseData)
+
 	return nil
 }
 
 // https://core.telegram.org/bots/api#sendchataction
-func SendChatAction[ID int64 | string](ctx context.Context, msg method_dto.SendChatAction[ID]) error {
-	data, err := json.Marshal(msg)
+func SendChatAction[ID int64 | string](ctx context.Context, data method_dto.SendChatAction[ID]) error {
+	marshalBytes, err := json.Marshal(data)
 	if err != nil {
 		return err
 	}
-	response, err := request.RequestWithContextAndData(ctx, request.Get, GetUrl()+"/sendChatAction", data)
+
+	response, err := request.RequestWithContextAndData(ctx, request.Get, GetUrl()+"/sendChatAction", marshalBytes)
 	if err != nil {
 		return err
 	}
-	responseJson, err := request.ResponseHandler(response)
+
+	responseData, err := request.ResponseHandler(response)
 	if err != nil {
 		return err
 	}
-	slog.Debug("info", "response", responseJson)
+
+	slog.Debug("info", "response", responseData)
+
 	return nil
 }
 
 // https://core.telegram.org/bots/api#senddice
-func SendDice[ID int64 | string](ctx context.Context, msg method_dto.SendDice[ID]) error {
-	data, err := json.Marshal(msg)
+func SendDice[ID int64 | string](ctx context.Context, data method_dto.SendDice[ID]) error {
+	marshalBytes, err := json.Marshal(data)
 	if err != nil {
 		return err
 	}
-	response, err := request.RequestWithContextAndData(ctx, request.Get, GetUrl()+"/sendDice", data)
+
+	response, err := request.RequestWithContextAndData(ctx, request.Get, GetUrl()+"/sendDice", marshalBytes)
 	if err != nil {
 		return err
 	}
-	responseJson, err := request.ResponseHandler(response)
+
+	responseData, err := request.ResponseHandler(response)
 	if err != nil {
 		return err
 	}
-	slog.Debug("info", "response", responseJson)
+
+	slog.Debug("info", "response", responseData)
+
 	return nil
 }
